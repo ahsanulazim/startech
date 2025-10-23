8418ef0b6dbd727c257342699dfb7d064f8b0629
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-10-23 15:41:50 +0600

    navbar responsive part-1

--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Drawer from "./Drawer";
 import Search from "../ui/Search";
-import { FaFireAlt, FaSmile, FaUser } from "react-icons/fa";
+import { FaFireAlt, FaSearch, FaShoppingBasket, FaSmile, FaUser } from "react-icons/fa";
 import Profile from "../ui/Profile";
 
 export default function Navbar() {
@@ -19,26 +19,30 @@ export default function Navbar() {
                 {/* Navbar */}
                 <header className=" bg-main">
                     <div className="navbar max-w-xxl mx-auto gap-x-5">
-                        <div className="flex-none lg:hidden">
+                        <div className="w-1/2 lg:hidden">
                             <label htmlFor="my-drawer-2" aria-label="open sidebar" className="btn btn-square btn-ghost">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="size-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
 
                             </label>
                         </div>
-                        <Link href="/" className="hidden lg:block"><img className="w-full max-w-28" src="/startech.svg" alt="startech" /></Link>
-                        <div className="navbar-center lg:hidden flex">
+                        <Link href="/" className=""><img className="w-full min-w-20 max-w-28" src="/startech.svg" alt="startech" /></Link>
+                        {/* <div className="navbar-center lg:hidden flex">
                             <Link href="/"><img className="w-full max-w-28" src="/startech.svg" alt="startech" /></Link>
-                        </div>
+                        </div> */}
                         <div className="navbar-center hidden lg:flex grow">
                             <Search />
                         </div>
-                        <div className="navbar-end w-auto">
-                            <div className="flex gap-x-5">
+                        <div className="navbar-end lg:w-auto">
+                            <div className="hidden lg:flex gap-x-5">
                                 {impLinks.map((links) => (
                                     <Profile key={links.id} icon={links.icon} title={links.title} subtitle={links.subtitle} href={links.href} />
                                 ))}
                             </div>
-                            <Link href="#" className=" ml-5"><button className="btn animatedBtn">PC Builder</button></Link>
+                            <Link href="#" className="hidden lg:block ml-5"><button className="btn animatedBtn">PC Builder</button></Link>
+                            <div className="join lg:hidden text-white gap-x-5">
+                                <button><FaSearch /></button>
+                                <button className="text-xl"><FaShoppingBasket /></button>
+                            </div>
                         </div>
                     </div>
                 </header>
