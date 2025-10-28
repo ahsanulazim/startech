0d984fa094e72ad4e73846a2f1380d5acf728c84
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-10-28 21:59:55 +0600

    comparision

--- components/home/Comparison.jsx
+++ components/home/Comparison.jsx
@@ -3,16 +3,22 @@ import Section from "../ui/Section";
 import Title from "../ui/Title";
 
 export default function Comparison() {
+  const t = {
+    heading: "Featured Comparisons",
+    subHeading: "Compare & Choose Your Desired Product!",
+  };
 
-    const t = {
-        heading: "Featured Comparisons",
-        subHeading: "Compare & Choose Your Desired Product!"
-    }
-
-    return (
-        <Section>
-            <Title heading={t.heading} subHeading={t.subHeading} />
-            <ComparisionCard />
-        </Section>
-    )
+  return (
+    <Section>
+      <Title heading={t.heading} subHeading={t.subHeading} />
+      <div className="grid grid-cols-3 gap-5">
+        <ComparisionCard />
+        <ComparisionCard />
+        <ComparisionCard />
+        <ComparisionCard />
+        <ComparisionCard />
+        <ComparisionCard />
+      </div>
+    </Section>
+  );
 }
--- components/shop/ComparisionCard.jsx
+++ components/shop/ComparisionCard.jsx
@@ -1,34 +1,40 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function ComparisionCard() {
+  const [products, setProducts] = useState([]);
+  const selectedIds = [1, 3];
 
-    const [products, setProducts] = useState([]);
-    const selectedIds = [1, 3];
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const responses = await Promise.all(
+        selectedIds.map((id) =>
+          fetch(`https://fakestoreapi.com/products/${id}`).then((res) =>
+            res.json()
+          )
+        )
+      );
+      setProducts(responses);
+    };
+    fetchProducts();
+  }, []);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const responses = await Promise.all(
-                selectedIds.map(id =>
-                    fetch(`https://fakestoreapi.com/products/${id}`).then(res => res.json())
-                )
-            );
-            setProducts(responses);
-        }
-        fetchProducts();
-    }, [])
-
-    return (
-        <div className="bg-base-100 rounded-md">
-            {products ? products.map(p => (
-                <div key={p.id}>
-                    <img src={p.image} alt={p.title} />
-                    <h3>{p.title}</h3>
-                    <p>{p.price}</p>
-                </div>
-            )) : <div>loading..</div>}
-
-        </div>
-    )
+  return (
+    <div className="bg-base-100 rounded-md">
+      <div></div>
+      {products ? (
+        products.map((p) => (
+          <div key={p.id}>
+            <img src={p.image} alt={p.title} />
+            <h3>{p.title}</h3>
+            <p>{p.price}</p>
+          </div>
+        ))
+      ) : (
+        <div>loading..</div>
+      )}
+      <button className="btn btn-soft btn-primary">Primary</button>
+    </div>
+  );
 }
