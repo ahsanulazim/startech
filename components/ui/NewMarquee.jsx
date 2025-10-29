a4926b3cb6cbd15278433ca6430ee22890906244
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-10-29 16:00:49 +0600

    added bottom navbar 2

--- components/ui/NewMarquee.jsx
+++ components/ui/NewMarquee.jsx
@@ -2,13 +2,15 @@ import Marquee from "react-fast-marquee";
 import Section from "./Section";
 
 export default function NewMarquee() {
-    return (
-        <Section>
-            <div className="p-2 bg-base-100 rounded-lg">
-                <Marquee className="text-sm">
-                    Sunday, 26 October, all our branches are open except IDB Branch. Additionally, our online activities are open and & operational.
-                </Marquee>
-            </div>
-        </Section>
-    )
+  return (
+    <Section>
+      <div className="p-2 bg-base-100 rounded-lg">
+        <Marquee className="text-sm">
+          {" "}
+          Sunday, 26 October, all our branches are open except IDB Branch.
+          Additionally, our online activities are open and & operational.
+        </Marquee>
+      </div>
+    </Section>
+  );
 }
