24dd49b8d0dc2be2a45f9fa777460fe211018fe1
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-11-05 00:35:46 +0600

    added firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: "startech-59a1b.firebaseapp.com",
+    projectId: "startech-59a1b",
+    storageBucket: "startech-59a1b.firebasestorage.app",
+    messagingSenderId: "57258748365",
+    appId: "1:57258748365:web:ab67aaac99d34553097c84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
