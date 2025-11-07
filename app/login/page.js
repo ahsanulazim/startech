4c9a74f4c7cda9264c2427319f7341a471b5f0ca
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-11-07 22:02:45 +0600

    login page access restriction 3

--- app/login/page.js
+++ app/login/page.js
@@ -3,18 +3,29 @@
 import UserForm from "@/components/auth/UserForm";
 import Breadcrumbs from "@/components/ui/Breadcrumbs";
 import Section from "@/components/ui/Section";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SiteContext } from "../context/MyContext";
 import { useRouter } from "next/navigation";
 
 export default function Login() {
 
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const { currentUser } = useContext(SiteContext);
 
   useEffect(() => {
-    if (currentUser) router.replace("/dashboard");
+    if (currentUser === undefined) return; // still loading context
+    if (currentUser) {
+      router.replace("/dashboard");
+    } else {
+      setLoading(false);
+    }
   }, [currentUser, router]);
+
+  if (loading) {
+    return <div>loading login...</div>;
+  }
+
 
   return (
     <main className="bg-base-300">
