c1d4dcf0fcbc01a0459b58eed593fa984fccd2c1
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-11-07 21:28:01 +0600

    login page access restriction

--- app/login/page.js
+++ app/login/page.js
@@ -3,39 +3,27 @@
 import UserForm from "@/components/auth/UserForm";
 import Breadcrumbs from "@/components/ui/Breadcrumbs";
 import Section from "@/components/ui/Section";
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { SiteContext } from "../context/MyContext";
 import { useRouter } from "next/navigation";
 
 export default function Login() {
-  const [loading, setLoading] = useState(true);
+
   const router = useRouter();
   const { currentUser } = useContext(SiteContext);
 
   useEffect(() => {
-    // Once context is hydrated, stop loading
-    setLoading(false);
-
-    if (currentUser) {
-      router.push("/dashboard");
-    }
+    if (currentUser) router.replace("/dashboard");
   }, [currentUser, router]);
 
-  if (loading) {
-    return <div>loading...</div>;
-  }
-
-  if (!currentUser) {
-    return (
-      <main className="bg-base-300">
-        <Section>
-          <Breadcrumbs title="Login" />
-        </Section>
-        <Section>
-          <UserForm login={true} />
-        </Section>
-      </main>
-    );
-  }
-  return null;
+  return (
+    <main className="bg-base-300">
+      <Section>
+        <Breadcrumbs title="Login" />
+      </Section>
+      <Section>
+        <UserForm login={true} />
+      </Section>
+    </main>
+  );
 }
